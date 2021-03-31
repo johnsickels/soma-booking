3983162f9971ee1f2dbb2b491c853ecec9e63c18
johnSickels <46609057+johnsickels@users.noreply.github.com>
2021-03-31 19:49:27 -0400

    more tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,33 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "./App";
 
 test("renders SOMA Booking header", () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/SOMA Booking/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("renders subheading", () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Book with ease./i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("type into name", () => {
+  render(<App />);
+  userEvent.type(
+    screen.getByPlaceholderText("Reservation Name"),
+    "Room for two"
+  );
+  expect(screen.getByPlaceholderText("Reservation Name")).toHaveValue(
+    "Room for two"
+  );
+});
+
+test("type into phone", () => {
+  render(<App />);
+  userEvent.type(screen.getByPlaceholderText("Phone Number"), "1234567890");
+  expect(screen.getByPlaceholderText("Phone Number")).toHaveValue("1234567890");
 });
