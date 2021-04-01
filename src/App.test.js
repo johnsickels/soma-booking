86e1c9ac76c63e7c0456d82ccf1df1c90dd41d4a
johnSickels <46609057+johnsickels@users.noreply.github.com>
2021-04-01 14:33:26 -0400

    optimize tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,21 +2,24 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import App from "./App";
+import NameInput from "./components/BookingForm/NameInput";
+import PhoneNumberInput from "./components/BookingForm/PhoneNumberInput";
+import NumberOfOccupantsInput from "./components/BookingForm/NumberOfOccupantsInput";
 
-test("renders SOMA Booking header", () => {
+test("Renders SOMA Booking header", () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/SOMA Booking/i);
   expect(linkElement).toBeInTheDocument();
 });
 
-test("renders subheading", () => {
+test("Renders subheading", () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/Book with ease./i);
   expect(linkElement).toBeInTheDocument();
 });
 
-test("type into name", () => {
-  render(<App />);
+test("Types into name", () => {
+  render(<NameInput />);
   userEvent.type(
     screen.getByPlaceholderText("Reservation Name"),
     "Room for two"
@@ -26,8 +29,8 @@ test("type into name", () => {
   );
 });
 
-test("type into phone", () => {
-  render(<App />);
+test("Types into phone", () => {
+  render(<PhoneNumberInput />);
   userEvent.type(screen.getByPlaceholderText("Phone Number"), "1234567890");
   expect(screen.getByPlaceholderText("Phone Number")).toHaveValue("1234567890");
 });
